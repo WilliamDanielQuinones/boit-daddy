ec23d851776b735f6665ed0e334710fda33752bd
WilliamDanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2020-08-30 02:21:26 -0400

    Change musicbot implementation to lib

--- index.js
+++ index.js
@@ -8,131 +8,44 @@ const ytdl = require('ytdl-core') //youtube video donwloader
 const client = new Discord.Client()
 
 const queue = new Map()
+// Require the Discord.js library.
+const Discord = require('discord.js');
 
-client.once('ready', () => {
-  console.log('Ready!')
- })
- client.once('reconnecting', () => {
-  console.log('Reconnecting!')
- })
- client.once('disconnect', () => {
-  console.log('Disconnect!')
- })
+// Start a new Client from Discord.js. You can name this to whatever you like.
+const client = new Discord.Client();
 
- client.on('message', async message => {
-  if (message.author.bot) return
-  if (!message.content.startsWith(prefix)) return
+// Put the Music module in the new Client object.
+// This allows for easy access to all the modules
+// functions and data.
+client.music = require("discord.js-musicbot-addon");
+  // Now we start the music module.
+  // Following the previous example.
+  client.music.start(client, {
+    // Set the api key used for YouTube.
+    youtubeKey: "YouTubeAPIKeyHere",
 
-  const serverQueue = queue.get(message.guild.id)
+    // The PLAY command Object.
+    play: {
+      // Usage text for the help command.
+      usage: "{{prefix}}play some tunes",
+      // Whether or not to exclude the command from the help command.
+      exclude: false
+    },
 
-  if (message.content.startsWith(`${prefix}play`)) {
-      execute(message, serverQueue)
-      return
-  } else if (message.content.startsWith(`${prefix}skip`)) {
-      skip(message, serverQueue)
-      return
-  } else if (message.content.startsWith(`${prefix}stop`)) {
-      stop(message, serverQueue)
-      return
-  } else {
-      message.channel.send("You need to enter a valid command!")
-  }
+    // Make it so anyone in the voice channel can skip the
+    // currently playing song.
+    anyoneCanSkip: true,
 
-})
+    // Make it so the owner (you) bypass permissions for music.
+    ownerOverMember: true,
+    ownerID: "yourDiscordId",
 
-async function execute(message, serverQueue) {
-  const args = message.content.split(" ")
+    // The cooldown Object.
+    cooldown: {
+      // This disables the cooldown. Not recommended.
+      enabled: false
+    }
+  });
 
-  const voiceChannel = message.member.voice.channel
-  if (!voiceChannel)
-    return message.channel.send(
-      "You need to be in a voice channel to play music!"
-    )
-  const permissions = voiceChannel.permissionsFor(message.client.user)
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return message.channel.send(
-      "I need the permissions to join and speak in your voice channel!"
-    )
-  }
-
-  const songInfo = await ytdl.getInfo(args[1])
-  const song = {
-  title: songInfo.title,
-  url: songInfo.video_url,
-  }
-
-  if (!serverQueue) {
-
-  }else {
-   serverQueue.songs.push(song)
-   console.log(serverQueue.songs)
-   return message.channel.send(`${song.title} has been added to the queue!`)
-  }
-
-  // Creating the contract for our queue
-  const queueContruct = {
-    textChannel: message.channel,
-    voiceChannel: voiceChannel,
-    connection: null,
-    songs: [],
-    volume: 5,
-    playing: true,
-  }
-  // Setting the queue using our contract
-  queue.set(message.guild.id, queueContruct)
-  // Pushing the song to our songs array
-  queueContruct.songs.push(song)
-
-  try {
-    // Here we try to join the voicechat and save our connection into our object.
-    var connection = await voiceChannel.join()
-    queueContruct.connection = connection
-    // Calling the play function to start a song
-    play(message.guild, queueContruct.songs[0])
-  } catch (err) {
-    // Printing the error message if the bot fails to join the voicechat
-    console.log(err)
-    queue.delete(message.guild.id)
-    return message.channel.send(err)
-  }
-}
-
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id)
-  if (!song) {
-    serverQueue.voiceChannel.leave()
-    queue.delete(guild.id)
-    return
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-        serverQueue.songs.shift()
-        play(guild, serverQueue.songs[0])
-    })
-    .on("error", error => console.error(error))
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`)
-}
-
-function skip(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    )
-  if (!serverQueue)
-    return message.channel.send("There is no song that I could skip!")
-  serverQueue.connection.dispatcher.end()
-}
-
-function stop(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    )
-  serverQueue.songs = []
-  serverQueue.connection.dispatcher.end()
-}
-
-client.login(token)
+// Connect the bot with your Discord applications bot token.
+client.login(token);
