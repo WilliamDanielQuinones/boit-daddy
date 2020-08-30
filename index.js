c2a61b14b0545027a55ea6029c2b2b6c7e6fdb4a
WilliamDanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2020-08-30 02:45:11 -0400

    Use new music lib

--- index.js
+++ index.js
@@ -1,45 +1,83 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const {
-	prefix,
-  token,
-  yt_key,
-  discord_id
-} = require('./config.json')
+/**
+ * Module Imports
+ */
+const { Client, Collection } = require("discord.js");
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json");
 
+const client = new Client({ disableMentions: "everyone" });
 
-// Put the Music module in the new Client object.
-// This allows for easy access to all the modules
-// functions and data.
-client.music = require("discord.js-musicbot")
-  // Now we start the music module.
-  // Following the previous example.
-  client.music.start(client, {
-    // Set the api key used for YouTube.
-    youtubeKey: yt_key,
+client.login(TOKEN);
+client.commands = new Collection();
+client.prefix = PREFIX;
+client.queue = new Map();
+const cooldowns = new Collection();
+const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
-    // The PLAY command Object.
-    play: {
-      // Usage text for the help command.
-      usage: `${prefix}play some tunes`,
-      // Whether or not to exclude the command from the help command.
-      exclude: false
-    },
+/**
+ * Client Events
+ */
+client.on("ready", () => {
+  console.log(`${client.user.username} ready!`);
+  client.user.setActivity(`${PREFIX}help`);
+});
+client.on("warn", (info) => console.log(info));
+client.on("error", console.error);
 
-    // Make it so anyone in the voice channel can skip the
-    // currently playing song.
-    anyoneCanSkip: true,
+/**
+ * Import all commands
+ */
+const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(join(__dirname, "commands", `${file}`));
+  client.commands.set(command.name, command);
+}
 
-    // Make it so the owner (you) bypass permissions for music.
-    ownerOverMember: true,
-    ownerID: discord_id,
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
 
-    // The cooldown Object.
-    cooldown: {
-      // This disables the cooldown. Not recommended.
-      enabled: false
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(PREFIX)})\\s*`);
+  if (!prefixRegex.test(message.content)) return;
+
+  const [, matchedPrefix] = message.content.match(prefixRegex);
+
+  const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 1) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
+      );
     }
-  })
+  }
 
-// Connect the bot with your Discord applications bot token.
-client.login(token)
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("There was an error executing that command.").catch(console.error);
+  }
+});
