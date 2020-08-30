2c06d2f21affd8bafe662f3c2206f631d09ff264
WilliamDanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2020-08-30 02:27:12 -0400

    Clean up

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const Discord = require('discord.js')
 const {
 	prefix,
-	token,
+  token,
+  yt_key,
+  discord_id
 } = require('./config.json')
-const ytdl = require('ytdl-core') //youtube video donwloader
 
 const client = new Discord.Client()
 
-const queue = new Map()
 // Require the Discord.js library.
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
 // Start a new Client from Discord.js. You can name this to whatever you like.
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 // Put the Music module in the new Client object.
 // This allows for easy access to all the modules
 // functions and data.
-client.music = require("discord.js-musicbot-addon");
+client.music = require("discord.js-musicbot-addon")
   // Now we start the music module.
   // Following the previous example.
   client.music.start(client, {
     // Set the api key used for YouTube.
-    youtubeKey: "YouTubeAPIKeyHere",
+    youtubeKey: yt_key,
 
     // The PLAY command Object.
     play: {
       // Usage text for the help command.
-      usage: "{{prefix}}play some tunes",
+      usage: `${prefix}play some tunes`,
       // Whether or not to exclude the command from the help command.
       exclude: false
     },
@@ -38,14 +38,14 @@ client.music = require("discord.js-musicbot-addon");
 
     // Make it so the owner (you) bypass permissions for music.
     ownerOverMember: true,
-    ownerID: "yourDiscordId",
+    ownerID: discord_id,
 
     // The cooldown Object.
     cooldown: {
       // This disables the cooldown. Not recommended.
       enabled: false
     }
-  });
+  })
 
 // Connect the bot with your Discord applications bot token.
-client.login(token);
+client.login(token)
